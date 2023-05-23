db4ba9bc67ff1dd38113de1eaa4fb6f95904c9b5
Andrew LIU <andrewliuzihua@gmail.com>
2023-05-23 16:12:47 +1000

    update Builder/facedeBuilder.cs

--- Builder/facedeBuilder.cs
+++ Builder/facedeBuilder.cs
@@ -13,12 +13,18 @@ public class Personbuilder // facade
   
   public PersonJobBuilder Works => new PersonJobBuilder(person);
   public PersonAddressBuilder Lives => new PersonAddressBuilder(person);
+  
+  //Not very good but it would do the job to cast PersonBuilder to PersonBuilder
+  public static implicit operator Person(PersonBuilder pb)
+  {
+    return pb.person;
+  }
 }
 
 public class PersonAddressBuilder : PersonBuilder
 {
   public PersonAddressBuilder(Person person)
-  {>
+  {
     this.person = person;
   }
   
@@ -55,5 +61,17 @@ public class PersonJobBuilder : Personbuilder
   }
 }
 
-var pb = new Personbuilder()
-var person = pb.Lives().At("xxx").Works().As("xxx");
+public class Demo 
+{
+  var pb = new Personbuilder();
+  /**
+  * Because both all inherit from Personbuilder and Lives and Works are from Personbuilder
+  */
+  //But we obvious not get actually Getting Person class here
+  var person = pb.Lives().At("xxx").Works().As("xxx");
+  
+  //After adding the implicit operator
+  Person person = pb.Lives().At("xxx").Works().As("xxx");
+  //now person is real person
+}
+
