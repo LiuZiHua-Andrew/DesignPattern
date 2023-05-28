b717ddf8cf28f17eead9bf0f7d4565befeb9137e
Andrew LIU <andrewliuzihua@gmail.com>
2023-05-28 10:53:30 +1000

    update Prototype/M1_Clonable_is_bad.cs

---
+++ Prototype/M1_Clonable_is_bad.cs
@@ -0,0 +1,5 @@
+/* 
+IClonable is not a good option for deep copy
+1. The document didn't specify whether it is for deep copy or shallow copy
+2. In default, the array copy is shallow? implicily mean do shallow only for this interface? not sure
+*/
