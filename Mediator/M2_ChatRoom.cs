69005c98aac655498df8b2f9d084f67f226532eb
Andrew LIU <andrewliuzihua@gmail.com>
2023-06-01 23:10:37 +1000

    update Mediator/M2_ChatRoom.cs

---
+++ Mediator/M2_ChatRoom.cs
@@ -0,0 +1,60 @@
+public class Person
+{
+  public string Name;
+  public ChatRoom Room;
+  
+  private List<string> chatLog = new List<string>();
+  
+  public Person(string name)
+  {
+    Name = name;
+  }
+  
+  public void Say(string msg)
+  {
+    Room.Broadcast(Name, msg);
+  }
+  
+  public void PrivateMessage(string who, string message)
+  {
+    Room.Message(Name, who, message);
+  }
+  
+  public void Receive(string sender, string msg)
+  {
+    string s = $"{sender}: '{message}'";
+    chatLog.Add(d);
+    WriteLine($"[{Name}'s chat session] {s}");
+  }
+}
+
+/*
+Here the ChatRoom class plays the role of mediator,
+when Person tries to talk to another Person, they are not actually aware of other's
+*/
+public class ChatRoom
+{
+  private List<Person> people = new List<Person>();
+  
+  public void Join(Person p)
+  {
+    string joinMsg = $"{p.Name} joins the chat";
+    Broadcast("room", joinMsg);
+  }
+  
+  public void Broadcast(string source, string message)
+  {
+      foreach (var p in people)
+      {
+        if(p.Name != source)
+        {
+          p.Receive(source, message);
+        }
+      }
+  }
+  
+  public void Message(string source, string destination, string message)
+  {
+    people.FirstOrDefault(p => p.Name == destination)?.Receive(source, message);
+  }
+}
