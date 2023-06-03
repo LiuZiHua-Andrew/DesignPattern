3c80688790452e279f4b49a29e083a8498732963
Andrew LIU <andrewliuzihua@gmail.com>
2023-06-03 11:24:01 +1000

    update Mediator/M3_EventBroker.cs

--- Mediator/M3_EventBroker.cs
+++ Mediator/M3_EventBroker.cs
@@ -1,17 +1,35 @@
 
 public class Actor
 {
+  protected EventBroker broker;
   
+  pblic Actor(EventBroker broker)
+  {
+    this broker = broker;
+  }
 }
 
 public class FootballPlayer : Actor
 {
-  
+  public FootballPlayer(EventBroker broker) : base(broker)
+  {
+    
+  }
 }
 
 public class Footballcoatch : Actor
 {
-  
+  public Footballcoatch(EventBroker broker) : base(broker)
+  {
+    //Filter out PlayerscoredEvent from all IObservable event then pipe into some actions
+    broker.OfType<PlayerscoredEvent>().Subscribe(pe => 
+    {
+      if(pe.GoalsScored < 3)
+      {
+        WriteLine($"Coach: well done, {pe.Name}");
+      }
+    });
+  }
 }
 
 public class PlayerEvent
@@ -19,12 +37,12 @@ public class PlayerEvent
   public string Name {get;set;}
 }
 
-public class PlayerscoredEvent
+public class PlayerscoredEvent : PlayerEvent
 {
   public int GoalsScored {get;set;}
 }
 
-public class PlayerSentOffEVent
+public class PlayerSentOffEVent : PlayerEvent
 {
   public string Reason {get;set;}
 }
@@ -32,7 +50,17 @@ public class PlayerSentOffEVent
 //Mediator
 public class EventBroker : IObservable<PlayerEvent>
 {
+  private Subject<PlayerEvent> subscriptions = new Subject<PlayerEvent>();
   
+  public IDisposable Subscribe(IObservable<PlayerEvent> observer)
+  {
+    return subscriptions.Subscribe(observer);
+  }
+  
+  public void Publish(PlayerEvent pe)
+  {
+    subscriptions.OnNext(pe);
+  }
 }
 
 static class Program
